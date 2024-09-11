da983341232829c253e2133194e6244bc1029978
Alexander Sergeev <a79164220703@gmail.com>
2024-09-11 23:55:47 +0300

    add binary_tree package

---
+++ src/main/java/sergeev/alexander/algorithms/binary_tree/TreeNode.java
@@ -0,0 +1,22 @@
+package sergeev.alexander.algorithms.binary_tree;
+
+
+public class TreeNode <T> {
+
+    T value;
+    TreeNode<T> left;
+    TreeNode<T> right;
+
+    TreeNode(T value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    TreeNode(T value, TreeNode<T> left, TreeNode<T> right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+}
+
