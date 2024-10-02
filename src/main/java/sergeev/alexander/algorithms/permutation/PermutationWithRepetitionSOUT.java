61b7b4ccb8f1fe5def1471b2acd8fe053b506c34
Alexander Sergeev <a79164220703@gmail.com>
2024-10-02 23:27:04 +0300

    add permutation

---
+++ src/main/java/sergeev/alexander/algorithms/permutation/PermutationWithRepetitionSOUT.java
@@ -0,0 +1,24 @@
+package sergeev.alexander.algorithms.permutation;
+
+public class PermutationWithRepetitionSOUT {
+
+    public static void main(String[] args) {
+
+        String str = "ABC";
+        int n = 4;
+
+        permute(str, n, "");
+
+    }
+
+    public static void permute(String str, int n, String curr) {
+        if (curr.length() == n) {
+            System.out.println(curr);
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            permute(str, n, curr + str.charAt(i));
+        }
+    }
+}
+
---
+++ src/main/java/sergeev/alexander/algorithms/permutation/PermutationWithoutRepetitionSOUT.java
@@ -0,0 +1,26 @@
+package sergeev.alexander.algorithms.permutation;
+
+public class PermutationWithoutRepetitionSOUT {
+
+    public static void main(String[] args) {
+
+        String str = "ABC";
+        boolean[] visited = new boolean[str.length()];
+        permute(str, visited, "")   ;
+
+    }
+
+    public static void permute(String str, boolean[] visited, String current) {
+        if (str.length() == current.length()) {
+            System.out.println(current);
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                permute(str, visited, current + str.charAt(i));
+                visited[i] = false;
+            }
+        }
+    }
+}
