e8ba593c0128e77c38ce592f275eb958cbf55d93
Alexander Sergeev <a79164220703@gmail.com>
2024-10-04 17:16:36 +0300

    add fibonacci iterative example

---
+++ src/main/java/sergeev/alexander/algorithms/fibonacci/FibonacciIterative.java
@@ -0,0 +1,22 @@
+package sergeev.alexander.algorithms.fibonacci;
+
+public class FibonacciIterative {
+
+    public static void main(String[] args) {
+
+        int n = 8;
+
+        System.out.println(fibonacciIterative(n));
+
+    }
+
+    public static long fibonacciIterative(int n) {
+        long[] arr = new long[n + 1];
+        arr[0] = 0L;
+        arr[1] = 1L;
+        for (int i = 2; i < n + 1; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n];
+    }
+}
