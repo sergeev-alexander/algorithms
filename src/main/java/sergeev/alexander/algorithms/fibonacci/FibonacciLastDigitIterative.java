72db723788cf12a8d0d4867102400dc4a9ad12ff
Alexander Sergeev <a79164220703@gmail.com>
2024-10-04 18:06:09 +0300

    add last digit of fibonacci number iterative example

---
+++ src/main/java/sergeev/alexander/algorithms/fibonacci/FibonacciLastDigitIterative.java
@@ -0,0 +1,21 @@
+package sergeev.alexander.algorithms.fibonacci;
+
+public class FibonacciLastDigitIterative {
+
+    public static void main(String[] args) {
+
+        int n = 841645; // index of fibonacci number
+
+        System.out.println(lastDigitOfFibonacciNumber(n)); // 5
+    }
+
+    public static int lastDigitOfFibonacciNumber(int index) {
+        int[] arr = new int[index + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i <= index; i++) {
+            arr[i] = (arr[i - 2] % 10) + (arr[i - 1] % 10);
+        }
+        return arr[index] % 10;
+    }
+}
