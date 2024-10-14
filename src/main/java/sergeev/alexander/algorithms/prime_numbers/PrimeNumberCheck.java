1fb9fcafb026b9d47edbd01a868e2e18b83d6fe8
Alexander Sergeev <a79164220703@gmail.com>
2024-10-14 19:58:46 +0300

    add prime number checks examples

---
+++ src/main/java/sergeev/alexander/algorithms/prime_numbers/PrimeNumberCheck.java
@@ -0,0 +1,39 @@
+package sergeev.alexander.algorithms.prime_numbers;
+
+import java.math.BigInteger;
+
+public class PrimeNumberCheck {
+
+    public static void main(String[] args) {
+
+        int num = 70;
+
+        System.out.println(isPrime(num));
+    }
+
+    private static boolean isPrime(int num) {
+        if (num <= 1)
+            return false;
+        if (num == 2 || num == 3)
+            return true;
+        if (num % 2 == 0 || num % 3 == 0)
+            return false;
+        for (int i = 5; i <= Math.sqrt(num); i += 6)
+            if (num % i == 0 || num % (i + 2) == 0)
+                return false;
+        return true;
+    }
+
+    public static boolean isPrime1(int num) {
+        if (num < 2) return false;
+        return new BigInteger(String.valueOf(num)).isProbablePrime((int) Math.log(num));
+    }
+
+    public static boolean isPrime_SLOWER(int num) {
+        if (num < 2) return false;
+        for (int i = 3; i < Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
