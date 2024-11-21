9c92a9c04d423ecf09611f085a6c068e1aab0ec1
Alexander Sergeev <a79164220703@gmail.com>
2024-11-21 23:05:47 +0300

    add small fixes

--- src/main/java/sergeev/alexander/algorithms/QuickSort.java
+++ src/main/java/sergeev/alexander/algorithms/QuickSort.java
@@ -5,14 +5,12 @@ import java.util.Arrays;
 public class QuickSort {
 
     public static void main(String[] args) {
-
-        int[] arr = {2, 4, 6, 9, 7};
-
+        int[] arr = {2,1,3,3};
+        quickSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
-
     }
 
-    public static void quickSort1(int[] arr, int leftIndex, int rightIndex) {
+    public static void quickSort(int[] arr, int leftIndex, int rightIndex) {
         if (arr.length == 0 || leftIndex >= rightIndex) return;
         int pivot = arr[(leftIndex + rightIndex) / 2];
         int leftMarkerIndex = leftIndex;
@@ -28,8 +26,8 @@ public class QuickSort {
                 rightMarkerIndex--;
             }
         }
-        if (leftIndex < rightMarkerIndex) quickSort1(arr, leftIndex, rightMarkerIndex);
-        if (rightIndex > leftMarkerIndex) quickSort1(arr, leftMarkerIndex, rightIndex);
+        if (leftIndex < rightMarkerIndex) quickSort(arr, leftIndex, rightMarkerIndex);
+        if (rightIndex > leftMarkerIndex) quickSort(arr, leftMarkerIndex, rightIndex);
     }
 
 }
