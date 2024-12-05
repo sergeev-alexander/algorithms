697c37584f6c3ef149f2d96040bc495b76ce39b5
Alexander Sergeev <a79164220703@gmail.com>
2024-12-05 22:48:52 +0300

    add MergeSort

---
+++ src/main/java/sergeev/alexander/algorithms/sorting_algorithms/MergeSort.java
@@ -0,0 +1,48 @@
+package sergeev.alexander.algorithms.sorting_algorithms;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+        int[] arr = {8, 4, 7, 1, 2, 3, 0, 0, 0, 6, 9};
+        mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void mergeSort(int[] arr) {
+        int length = arr.length;
+        if (length == 1) return;
+        int mid = length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[length - mid];
+        for (int i = 0; i < left.length; i++) {
+            left[i] = arr[i];
+        }
+        for (int i = 0; i < right.length; i++) {
+            right[i] = arr[i + mid];
+        }
+        mergeSort(left);
+        mergeSort(right);
+        merge(arr, left, right);
+    }
+
+    private static void merge(int[] destArr, int[] leftArr, int[] rightArr) {
+        int leftLength = leftArr.length;
+        int rightLength = rightArr.length;
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int arrIndex = 0;
+        while (leftIndex < leftLength && rightIndex < rightLength) {
+            if (leftArr[leftIndex] < rightArr[rightIndex]) {
+                destArr[arrIndex++] = leftArr[leftIndex++];
+            } else destArr[arrIndex++] = rightArr[rightIndex++];
+        }
+        for (int i = leftIndex; i < leftLength; i++) {
+            destArr[arrIndex++] = leftArr[i];
+        }
+        for (int i = rightIndex; i < rightLength; i++) {
+            destArr[arrIndex++] = rightArr[i];
+        }
+    }
+}
