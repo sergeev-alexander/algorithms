d12b26274a2e1d6d298845ed24308505bd3f9ef6
Alexander Sergeev <a79164220703@gmail.com>
2024-12-05 23:31:39 +0300

    add InsertionSort

---
+++ src/main/java/sergeev/alexander/algorithms/sorting_algorithms/InsertionSort.java
@@ -0,0 +1,25 @@
+package sergeev.alexander.algorithms.sorting_algorithms;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        int[] arr = {8, 1, 7, 2, 6, 0, 0, 3, 5, 4};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int currentNum = arr[i];
+            int j = i;
+            while (j > 0 && arr[j - 1] > currentNum) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = currentNum;
+        }
+    }
+}
+
