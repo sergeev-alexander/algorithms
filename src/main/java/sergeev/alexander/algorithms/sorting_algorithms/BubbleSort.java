643a42b7fee16215a990abf438cdd2ceabb9efdb
Alexander Sergeev <a79164220703@gmail.com>
2024-12-10 18:13:52 +0300

    fixed QuickSort

--- src/main/java/sergeev/alexander/algorithms/sorting_algorithms/BubbleSort.java
+++ src/main/java/sergeev/alexander/algorithms/sorting_algorithms/BubbleSort.java
@@ -5,23 +5,25 @@ import java.util.Arrays;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        int[] arr = {6,5,7,4,8,0,2,3,1};
+        int[] arr = {6, 5, 7, 4, 8, 0, 2, 3, 1};
         bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
     public static void bubbleSort(int[] array) {
-        boolean isSorted = false;
-        while (!isSorted) {
-            isSorted = true;
-            for (int i = 1; i < array.length; i++) {
-                if (array[i] < array[i - 1]) {
+        int endOfSortPos = array.length;
+        int lastPos = 0;
+        while (endOfSortPos > 0) {
+            for (int i = 1; i < endOfSortPos; i++) {
+                if (array[i - 1] > array[i]) {
                     int temp = array[i];
                     array[i] = array[i - 1];
                     array[i - 1] = temp;
-                    isSorted = false;
+                    lastPos = i;
                 }
             }
+            endOfSortPos = lastPos;
+            lastPos = 0;
         }
     }
 }
