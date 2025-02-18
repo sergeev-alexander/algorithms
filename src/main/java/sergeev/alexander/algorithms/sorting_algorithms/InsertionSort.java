ac0cc5851c72038ac0e2277f434c0df0db804d9b
Alexander Sergeev <a79164220703@gmail.com>
2025-02-18 15:52:08 +0300

    Update InsertionSort.java

--- src/main/java/sergeev/alexander/algorithms/sorting_algorithms/InsertionSort.java
+++ src/main/java/sergeev/alexander/algorithms/sorting_algorithms/InsertionSort.java
@@ -15,8 +15,7 @@ public class InsertionSort {
             int currentNum = arr[i];
             int j = i;
             while (j > 0 && arr[j - 1] > currentNum) {
-                arr[j] = arr[j - 1];
-                j--;
+                arr[j] = arr[--j];
             }
             arr[j] = currentNum;
         }
