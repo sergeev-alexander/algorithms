6bb1360ce0396d62c7ae966837f4b7d41f6890f7
Alexander Sergeev <a79164220703@gmail.com>
2025-05-28 18:25:23 +0300

    add solution to 1784 task

---
+++ src/main/java/sergeev/alexander/leetcode/a1784.java
@@ -0,0 +1,30 @@
+package sergeev.alexander.leetcode;
+
+// 1784. Check if Binary String Has at Most One Segment of Ones
+public class a1784 {
+
+    /*
+    Given a binary string s without leading zeros, return true if s contains at most one contiguous segment of ones.
+    Otherwise, return false.
+     */
+
+    public static void main(String[] args) {
+        String s = "100011";
+        System.out.println(checkOnesSegment(s));
+    }
+
+    // BEATS 100%
+    public static boolean checkOnesSegment(String s) {
+        boolean wasZero = false;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                wasZero = true;
+                continue;
+            }
+            if (wasZero && s.charAt(i) == '1') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
