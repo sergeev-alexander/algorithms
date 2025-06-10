fe3efc983b4c228ad71a2d4db483b0b32e625ef3
Alexander Sergeev <a79164220703@gmail.com>
2025-06-11 00:15:35 +0300

    add more solutions to 1832 task

--- src/main/java/sergeev/alexander/leetcode/a1832.java
+++ src/main/java/sergeev/alexander/leetcode/a1832.java
@@ -15,8 +15,54 @@ public class a1832 {
         System.out.println(checkIfPangram(s));
     }
 
-    // BEATS 65%
+    // BEATS 83%
     public static boolean checkIfPangram(String sentence) {
+        if (sentence.length() < 26) return false;
+        boolean[] arr = new boolean[26];
+        int count = 0;
+        for (int i = 0; i < sentence.length(); i++) {
+            int pos = sentence.charAt(i) - 'a';
+            if (!arr[pos]) {
+                arr[pos] = true;
+                count++;
+            }
+            if (count == 26) return true;
+        }
+        return false;
+    }
+
+    // BEATS 65%
+    public static boolean checkIfPangram4(String sentence) {
+        BitSet bitSet = new BitSet(26);
+        int count = 0;
+        for (int i = 0; i < sentence.length(); i++) {
+            int pos = sentence.charAt(i) - 'a';
+            if (!bitSet.get(pos)) {
+                count++;
+            }
+            if (count == 26) return true;
+            bitSet.set(pos);
+        }
+        return false;
+    }
+
+    // BEATS 83%
+    public static boolean checkIfPangram3(String sentence) {
+        boolean[] arr = new boolean[26];
+        int count = 0;
+        for (int i = 0; i < sentence.length(); i++) {
+            int pos = sentence.charAt(i) - 'a';
+            if (!arr[pos]) {
+                arr[pos] = true;
+                count++;
+            }
+            if (count == 26) return true;
+        }
+        return false;
+    }
+
+    // BEATS 65%
+    public static boolean checkIfPangram2(String sentence) {
         boolean[] arr = new boolean[26];
         int count = 0;
         for (int i = 0; i < sentence.length(); i++) {
