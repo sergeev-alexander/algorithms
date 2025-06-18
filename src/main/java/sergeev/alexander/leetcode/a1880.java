3fd47374432f9f2bb6b83f26d0a02dfa37228190
Alexander Sergeev <a79164220703@gmail.com>
2025-06-18 22:43:22 +0300

    add solution to 1880 task

--- src/main/java/sergeev/alexander/leetcode/a1880.java
+++ src/main/java/sergeev/alexander/leetcode/a1880.java
@@ -1,12 +1,23 @@
-package sergeev.alexander;
+package sergeev.alexander.leetcode;
 
-public class Temp {
+// 1880. Check if Word Equals Summation of Two Words
+public class a1880 {
+
+    /*
+    The letter value of a letter is its position in the alphabet starting from 0 (i.e. 'a' -> 0, 'b' -> 1, 'c' -> 2, etc.).
+    The numerical value of some string of lowercase English letters s is the concatenation of the letter values of each letter in s,
+    which is then converted into an integer.
+    For example, if s = "acb", we concatenate each letter's letter value, resulting in "021". After converting it, we get 21.
+    You are given three strings firstWord, secondWord, and targetWord, each consisting of lowercase English letters 'a' through 'j' inclusive.
+    Return true if the summation of the numerical values of firstWord and secondWord equals the numerical value of targetWord, or false otherwise.
+     */
 
     public static void main(String[] args) {
         String firstWord = "aaa", secondWord = "a", targetWord = "aab";
         System.out.println(isSumEqual(firstWord, secondWord, targetWord));
     }
 
+    // BEATS 100%
     public static boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
         int firstSum = 0;
 
