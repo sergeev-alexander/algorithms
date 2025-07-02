6020a9af9f9d905f4b30decb72c0f60a90216091
sergeev-alexander <a79164220703@gmail.com>
2025-07-02 22:53:32 +0300

    add solutions for 1893 task

--- src/main/java/sergeev/alexander/leetcode/a1893.java
+++ src/main/java/sergeev/alexander/leetcode/a1893.java
@@ -1,6 +1,14 @@
-package sergeev.alexander;
+package sergeev.alexander.leetcode;
 
-public class Temp {
+// 1893. Check if All the Integers in a Range Are Covered
+public class a1893 {
+
+    /*
+    You are given a 2D integer array ranges and two integers left and right.
+    Each ranges[i] = [starti, endi] represents an inclusive interval between starti and endi.
+    Return true if each integer in the inclusive range [left, right] is covered by at least one interval in ranges. Return false otherwise.
+    An integer x is covered by an interval ranges[i] = [starti, endi] if starti <= x <= endi.
+     */
 
     public static void main(String[] args) {
         int[][] arr = {{1,10}, {10,20}};
@@ -8,6 +16,7 @@ public class Temp {
         System.out.println(isCovered(arr, left, right));
     }
 
+    // BEATS 100%
     public static boolean isCovered(int[][] ranges, int left, int right) {
         for (int i = left; i <= right; i++) {
             boolean isCovered = false;
