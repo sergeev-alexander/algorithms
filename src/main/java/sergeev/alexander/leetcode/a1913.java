fa3549a5bb94a73d2170ce99b8fa70ac30375ea2
sergeev-alexander <a79164220703@gmail.com>
2025-07-07 20:05:20 +0300

    add one more solution for 1913 task

--- src/main/java/sergeev/alexander/leetcode/a1913.java
+++ src/main/java/sergeev/alexander/leetcode/a1913.java
@@ -3,7 +3,7 @@ package sergeev.alexander.leetcode;
 import java.util.Arrays;
 
 // 1913. Maximum Product Difference Between Two Pairs
-public class a1923 {
+public class a1913 {
 
     /*
     The product difference between two pairs (a, b) and (c, d) is defined as (a * b) - (c * d).
@@ -18,8 +18,34 @@ public class a1923 {
         System.out.println(maxProductDifference(arr));
     }
 
-    // BEATS 74%
+    // BEATS 99%
     public static int maxProductDifference(int[] nums) {
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num > max1) {
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max2 = num;
+            }
+
+            if (num < min1) {
+                min2 = min1;
+                min1 = num;
+            } else if (num < min2) {
+                min2 = num;
+            }
+        }
+
+        return (max1 * max2) - (min1 * min2);
+    }
+
+    // BEATS 74%
+    public static int maxProductDifference1(int[] nums) {
         Arrays.sort(nums);
         return (nums[nums.length - 1] * nums[nums.length - 2]) - (nums[0] * nums[1]);
     }
