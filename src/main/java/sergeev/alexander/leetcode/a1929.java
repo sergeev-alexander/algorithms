e7fa2bfc0d3225f1d23f6f35ded0e39906143326
sergeev-alexander <a79164220703@gmail.com>
2025-07-07 21:23:30 +0300

    add one more solution for 1929 task

--- src/main/java/sergeev/alexander/leetcode/a1929.java
+++ src/main/java/sergeev/alexander/leetcode/a1929.java
@@ -17,8 +17,17 @@ public class a1929 {
         System.out.println(Arrays.toString(getConcatenation(arr)));
     }
 
-    // BEATS 96%
+    // BEATS 100%
     public static int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] resultArray = new int[n * 2];
+        System.arraycopy(nums, 0,resultArray,0, n);
+        System.arraycopy(nums, 0,resultArray,n, n);
+        return resultArray;
+    }
+
+    // BEATS 96%
+    public static int[] getConcatenation1(int[] nums) {
         int n = nums.length;
         int[] resultArray = new int[n * 2];
 
