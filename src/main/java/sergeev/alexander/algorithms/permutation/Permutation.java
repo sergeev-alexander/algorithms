1784164b85ffa6743e697ea94884c4e4d4bd21bd
sergeev-alexander <a79164220703@gmail.com>
2025-07-14 21:10:29 +0300

    add iteractive permutation example

--- src/main/java/sergeev/alexander/algorithms/permutation/Permutation.java
+++ src/main/java/sergeev/alexander/algorithms/permutation/Permutation.java
@@ -11,12 +11,13 @@ public class Permutation {
     }
 
     // Recursive permutation
-    public static List<List<Integer>> permute(int[] nums) {
+    public static List<List<Integer>> permuteRecursive(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         generatePermutations(nums, 0, result);
         return result;
     }
 
+    // Recursive permutation
     public static void generatePermutations(int[] nums, int start, List<List<Integer>> result) {
         if (start == nums.length - 1) {
             List<Integer> list = new ArrayList<>();
@@ -35,6 +36,42 @@ public class Permutation {
         }
     }
 
+    // Iteractive permutation
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
+        int[] arr = new int[n];
+        result.add(toList(nums));
+
+        int i = 0;
+        while (i < n) {
+            if (arr[i] < i) {
+                if (i % 2 == 0) {
+                    swap(nums, 0, i); // Чётное i
+                } else {
+                    swap(nums, arr[i], i); // Нечётное i
+                }
+                result.add(toList(nums)); // Сохраняем новую перестановку
+                arr[i]++; // Увеличиваем счётчик
+                i = 0; // Сбрасываем для следующей итерации
+            } else {
+                arr[i] = 0;
+                i++;
+            }
+        }
+        return result;
+    }
+
+    // Utility method
+    private static List<Integer> toList(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            list.add(num);
+        }
+        return list;
+    }
+
+    // Utility method
     public static void swap(int[] nums, int start, int i) {
         int temp = nums[i];
         nums[i] = nums[start];
