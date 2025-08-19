5ef1369bfd4eea10fcf0b2c176e3ba6dc604f4c6
sergeev-alexander <a79164220703@gmail.com>
2025-08-19 18:52:25 +0300

    add solution to 73 task

---
+++ src/main/java/sergeev/alexander/leetcode/a73.java
@@ -0,0 +1,46 @@
+package sergeev.alexander.leetcode;
+
+import java.util.Arrays;
+
+// 73. Set Matrix Zeroes
+public class a73 {
+
+    /*
+    Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+    You must do it in place.
+     */
+
+    public static void main(String[] args) {
+        int[][] mat = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+        setZeroes(mat);
+        for (int[] arr : mat) {
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+    // BEATS 72%
+    public static void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean[] iArr = new boolean[m];
+        boolean[] jArr = new boolean[n];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    iArr[i] = true;
+                    jArr[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (iArr[i] || jArr[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
