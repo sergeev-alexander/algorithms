7732ca5cb8ef865e61657787ae5c71c2f6b4235e
sergeev-alexander <a79164220703@gmail.com>
2025-09-04 22:35:15 +0300

    add solution to 2231 task

--- src/main/java/sergeev/alexander/leetcode/a2231.java
+++ src/main/java/sergeev/alexander/leetcode/a2231.java
@@ -14,27 +14,48 @@ public class a2231 {
      */
 
     public static void main(String[] args) {
-        int num = 2481;
+        int num = 1234;
         System.out.println(largestInteger(num));
     }
 
-    // BEATS 80%
+    // BEATS 96%
     public static int largestInteger(int num) {
+        char[] arr = String.valueOf(num).toCharArray();
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if ((arr[i] % 2 == 0 && arr[j] % 2 == 0 || arr[i] % 2 != 0 && arr[j] % 2 != 0) && arr[j] > arr[i]) {
+                    char temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        return Integer.parseInt(new String(arr));
+    }
+
+    // BEATS 80%
+    public static int largestInteger1(int num) {
         List<Character> oddList = new ArrayList<>();
         List<Character> evenList = new ArrayList<>();
         char[] arr = String.valueOf(num).toCharArray();
+
         for (char ch : arr) {
             if (ch % 2 == 0) evenList.add(ch);
             else oddList.add(ch);
         }
+
         oddList.sort(Comparator.reverseOrder());
         evenList.sort(Comparator.reverseOrder());
+
         int evenCount = 0;
         int oddCount = 0;
+
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 == 0) arr[i] = evenList.get(evenCount++);
             else arr[i] = oddList.get(oddCount++);
         }
+
         return Integer.parseInt(new String(arr));
     }
 }
