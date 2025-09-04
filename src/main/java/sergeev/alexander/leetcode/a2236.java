ebaec2e3775b46ba62fa61327aed2944c589305d
sergeev-alexander <a79164220703@gmail.com>
2025-09-04 22:49:47 +0300

    add solution to 2236 task

---
+++ src/main/java/sergeev/alexander/leetcode/a2236.java
@@ -0,0 +1,42 @@
+package sergeev.alexander.leetcode;
+
+// 2236. Root Equals Sum of Children
+public class a2236 {
+
+    /*
+    You are given the root of a binary tree that consists of exactly 3 nodes: the root, its left child, and its right child.
+    Return true if the value of the root is equal to the sum of the values of its two children, or false otherwise.
+     */
+
+    public static void main(String[] args) {
+        TreeNode head =
+                new TreeNode(10,
+                        new TreeNode(4),
+                        new TreeNode(6));
+
+        System.out.println(checkTree(head));
+    }
+
+    public static boolean checkTree(TreeNode root) {
+        return root.val == root.left.val + root.right.val;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
