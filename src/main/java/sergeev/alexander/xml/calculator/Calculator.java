5ebab88b68b5c66d823fbe9d583afd673b40e55a
sergeev-alexander <a79164220703@gmail.com>
2025-09-17 22:06:35 +0300

    small fix

--- src/main/java/sergeev/alexander/xml/calculator/Calculator.java
+++ src/main/java/sergeev/alexander/xml/calculator/Calculator.java
@@ -2,10 +2,10 @@ package sergeev.alexander.xml.calculator;
 
 import java.time.LocalDate;
 
-public class Temp2 {
+public class Calculator {
 
     public static void main(String[] args) {
-        LocalDate start = LocalDate.of(2025, 3, 14);
+        LocalDate start = LocalDate.of(2025, 7, 22);
         for (int i = 0; i < 12; i++) {
             start = get(start);
             System.out.println(start);
