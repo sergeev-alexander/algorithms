668cead2736d2c949bb0f2bc289f8dc6e427879d
sergeev-alexander <a79164220703@gmail.com>
2025-10-03 16:59:40 +0300

    add more solutions to 938 task

--- src/main/java/sergeev/alexander/leetcode/a938.java
+++ src/main/java/sergeev/alexander/leetcode/a938.java
@@ -1,5 +1,9 @@
 package sergeev.alexander.leetcode;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 // 938. Range Sum of BST
 public class a938 {
 
@@ -21,8 +25,87 @@ public class a938 {
         System.out.println(rangeSumBST(root, low, high));
     }
 
-    // BEATS 43%
+    // BEATS 100%
     public static int rangeSumBST(TreeNode root, int low, int high) {
+        return ((root != null && root.val <= high && root.val >= low) ? root.val : 0)
+                + ((root != null && root.val > low) ? rangeSumBST(root.left, low, high) : 0)
+                + ((root != null && root.val < high) ? rangeSumBST(root.right, low, high) : 0);
+    }
+
+
+    // BETAS 100%
+    public static int rangeSumBST4(TreeNode root, int low, int high) {
+        if (root == null) return 0;
+
+        int sum = 0;
+
+        if (root.val <= high && root.val >= low) {
+            sum += root.val;
+        }
+
+        if (root.val > low) {
+            sum += rangeSumBST(root.left, low, high);
+        }
+
+        if (root.val < high) {
+            sum += rangeSumBST(root.right, low, high);
+        }
+
+        return sum;
+    }
+
+    // BEATS 6%
+    public static int rangeSumBST3(TreeNode root, int low, int high) {
+        int sum = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+
+            if (current.val <= high && current.val >= low) {
+                sum += current.val;
+            }
+
+            if (current.left != null) {
+                queue.offer(current.left);
+            }
+
+            if (current.right != null) {
+                queue.offer(current.right);
+            }
+        }
+
+        return sum;
+    }
+
+    // BEATS 6%
+    public static int rangeSumBST2(TreeNode root, int low, int high) {
+        int sum = 0;
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.offer(root);
+
+        while (!deque.isEmpty()) {
+            TreeNode current = deque.poll();
+
+            if (current.val <= high && current.val >= low) {
+                sum += current.val;
+            }
+
+            if (current.left != null) {
+                deque.offer(current.left);
+            }
+
+            if (current.right != null) {
+                deque.offer(current.right);
+            }
+        }
+
+        return sum;
+    }
+
+    // BEATS 43%
+    public static int rangeSumBST1(TreeNode root, int low, int high) {
         int sum = 0;
 
         if (root.val <= high && root.val >= low) {
